26cc87ea38aa093f8bbe8efedd7d77dca2ec4d19
Abby Christens <achristens@gmail.com>
2017-09-19 15:40:31 -0400

    Exercise 4

--- exercise.rb
+++ exercise.rb
@@ -3,8 +3,6 @@ def double(my_number)
   return my_number * 2
 end
 
-double(90)
-
 # exercise 2
 def negative?(number)
   return number < 0
@@ -12,5 +10,14 @@ end
 
 # exercise 3
 def is_even?(number)
-  return number % 2 == 0
+  number % 2 == 0
+end
+
+# exercise 4
+def char_number(string)
+   if string.size < 8
+     return false
+   else
+     return true
+   end
 end
